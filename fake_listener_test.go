d6871a6120390de4c572b827e49773666d18d8c1
Denis Davydov <follenlast2@gmail.com>
2023-08-21 14:11:47 +0300

    fake listener for tests

--- fake_listener_test.go
+++ fake_listener_test.go
@@ -7,57 +7,73 @@ import (
 	watermillnet "github.com/andyollylarkin/watermill-net"
 )
 
-type listenerStub struct{}
-
-func (ls *listenerStub) Accept() (watermillnet.Connection, error) { return nil, nil }
-func (ls *listenerStub) Close() error                             { return nil }
-func (ls *listenerStub) Addr() net.Addr                           { return nil }
-
-type listenerStub2 struct {
-	conn net.Conn
+type listenerStub struct {
+	Conn watermillnet.Connection
 }
 
+func NewListenerStub(conn net.Conn) *listenerStub {
+	s := new(listenerStub)
+	s.Conn = NewConnWrapper(conn)
+
+	return s
+}
+
+func NewListenerStubWithFakeConn(conn watermillnet.Connection) *listenerStub {
+	s := new(listenerStub)
+	s.Conn = conn
+
+	return s
+}
+
+func (ls *listenerStub) Accept() (watermillnet.Connection, error) {
+	return ls.Conn, nil
+}
+func (ls *listenerStub) Close() error   { return ls.Conn.Close() }
+func (ls *listenerStub) Addr() net.Addr { return ls.Conn.LocalAddr() }
+
 type connWrapper struct {
-	c net.Conn
+	C      net.Conn
+	Closed bool
 }
 
 func NewConnWrapper(conn net.Conn) *connWrapper {
 	cw := new(connWrapper)
-	cw.c = conn
+	cw.C = conn
 
 	return cw
 }
 
 func (pc *connWrapper) Read(b []byte) (n int, err error) {
-	return pc.c.Read(b)
+	return pc.C.Read(b)
 }
 
 func (pc *connWrapper) Write(b []byte) (n int, err error) {
-	return pc.c.Write(b)
+	return pc.C.Write(b)
 }
 
 func (pc *connWrapper) Close() error {
-	return pc.c.Close()
+	pc.Closed = true
+	return pc.C.Close()
 }
 
 func (pc *connWrapper) LocalAddr() net.Addr {
-	return pc.c.LocalAddr()
+	return pc.C.LocalAddr()
 }
 
 func (pc *connWrapper) RemoteAddr() net.Addr {
-	return pc.c.RemoteAddr()
+	return pc.C.RemoteAddr()
 }
 
 func (pc *connWrapper) SetDeadline(t time.Time) error {
-	return pc.c.SetDeadline(t)
+	return pc.C.SetDeadline(t)
 }
 
 func (pc *connWrapper) SetReadDeadline(t time.Time) error {
-	return pc.c.SetReadDeadline(t)
+	return pc.C.SetReadDeadline(t)
 }
 
 func (pc *connWrapper) SetWriteDeadline(t time.Time) error {
-	return pc.c.SetWriteDeadline(t)
+	return pc.C.SetWriteDeadline(t)
 }
 
 func (pc *connWrapper) Connect(addr net.Addr) error {
