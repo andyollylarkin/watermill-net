10eb3c73fe2195d992e8ed143c60deb85e995957
Denis Davydov <follenlast2@gmail.com>
2023-08-28 09:39:28 +0300

    mutex for tcp4 listener

--- pkg/connection/tcp4Listener.go
+++ pkg/connection/tcp4Listener.go
@@ -2,6 +2,7 @@ package connection
 
 import (
 	"net"
+	"sync"
 
 	watermillnet "github.com/andyollylarkin/watermill-net"
 )
@@ -9,6 +10,7 @@ import (
 type TCP4Listener struct {
 	l    net.Listener
 	conn watermillnet.Connection
+	mu   sync.Mutex
 }
 
 func NewTCP4Listener(l net.Listener) *TCP4Listener {
@@ -20,6 +22,9 @@ func NewTCP4Listener(l net.Listener) *TCP4Listener {
 
 // Accept waits for and returns the next connection to the listener.
 func (tl *TCP4Listener) Accept() (watermillnet.Connection, error) {
+	tl.mu.Lock()
+	defer tl.mu.Unlock()
+
 	conn, err := tl.l.Accept()
 
 	if err != nil {
@@ -38,10 +43,16 @@ func (tl *TCP4Listener) Accept() (watermillnet.Connection, error) {
 // Close closes the listener.
 // Any blocked Accept operations will be unblocked and return errors.
 func (tl *TCP4Listener) Close() error {
+	tl.mu.Lock()
+	defer tl.mu.Unlock()
+
 	return tl.conn.Close()
 }
 
 // Addr returns the listener's network address.
 func (tl *TCP4Listener) Addr() net.Addr {
+	tl.mu.Lock()
+	defer tl.mu.Unlock()
+
 	return tl.conn.LocalAddr()
 }
